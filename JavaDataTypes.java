b4d75f9170aa657a6bbfaa5d863249a9ec34f0f3
shahadatrucse <shahadat.rucse@gmail.com>
2021-12-06 10:03:11 +0600

    Java data types.

---
+++ JavaDataTypes.java
@@ -0,0 +1,10 @@
+public class JavaDataTypes {
+    /* boolean data type
+    byte data type
+    char data type
+    short data type
+    int data type
+    long data type
+    float data type
+    double data type */
+}
