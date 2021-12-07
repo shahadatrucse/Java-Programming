796c6c9370b2afdfe6363eea1299c0c5ee37e76b
shahadatrucse <shahadat.rucse@gmail.com>
2021-12-07 06:49:29 +0600

    Java all type method.

---
+++ JavaMethod.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class JavaMethod {
+    /*public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number.");
+        int num=scan.nextInt();
+        System.out.println(num);
+    }
+
+     */
+    /* Accessor and Mutator in Java Method.
+    private int roll;
+    private String name;
+    public int getRoll(){
+        return roll;
+    }
+    public void setRoll(int roll){
+        this.roll=roll;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void display()
+    {
+        System.out.println("Roll Number = "+roll);
+        System.out.println("Student Name ="+name);
+    }
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+
+    }*/
+
+}
+
